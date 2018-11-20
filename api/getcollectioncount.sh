c92e9124d994877b8ebb1d6a3d01681654bb9d2c
Twan Goosen <twan@clarin.eu>
2018-11-20 10:52:31 +0100

    search api variable

--- api/getcollectioncount.sh
+++ api/getcollectioncount.sh
@@ -1,5 +1,5 @@
-#COLLECTION_ID="05815_L_RO_Folkmedia"
+#!/usr/bin/env bash
+source env.sh
 COLLECTION_ID=`echo $@|sed -e 's/ /%20/g'`
-KEY="DoLz47agJ"
-URL="https://www.europeana.eu/api/v2/search.json?wskey=${KEY}&query=europeana_collectionName:${COLLECTION_ID}&rows=1&profile=minimal"
+URL="${SEARCH_API}?wskey=${KEY}&query=europeana_collectionName:${COLLECTION_ID}&rows=1&profile=minimal"
 curl -s "$URL" | jq .totalResults
--- api/retrieve_sets_json.sh
+++ api/retrieve_sets_json.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source env.sh
-curl -L "https://www.europeana.eu/api/v2/search.json?wskey=${KEY}&query=europeana_collectionName:*&facet=europeana_collectionName&f.europeana_collectionName.facet.limit=4000&profile=facets&rows=0"
+curl -L "${SEARCH_API}?wskey=${KEY}&query=europeana_collectionName:*&facet=europeana_collectionName&f.europeana_collectionName.facet.limit=4000&profile=facets&rows=0"
 
