7b4432bfe8e900685b586bebf3a9cbac6289f9e6
Twan Goosen <twan@clarin.eu>
2019-02-08 11:08:18 +0100

    retrieve full text resource urls

--- fulltext/get-text-for-record.sh
+++ fulltext/get-text-for-record.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+#
+# Script to get full text for a Europeana record via the IIIF API
+#
+# Author: Twan Goosen <twan@clarin.eu>
+
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 if [ -e "${BASE_DIR}/env.sh" ]; then
@@ -12,22 +17,36 @@ if ! [ "${API_URL}" ]; then
         exit 1
 fi
 
+TMP_FILES=()
 
 
 print_usage() {
         echo "usage: $0 --collection <collectionId> --record <recordId>"
 }
 
-get_anno_pages() {
-	COLLECTION="$1" # e.g. 9200338
-	RECORD="$2" # e.g. BibliographicResource_3000117673618
+clean_up_tmp() {
+	for f in "${TMP_FILES[@]}"; do
+		if [ -e "$f" ]; then
+			rm "$f"
+		fi
+	done
+}
 
-	MANIFEST_URL="${API_URL}/presentation/${COLLECTION}/${RECORD}/manifest"
-	
-	if ! curl -sL "$MANIFEST_URL" | jq -r '.sequences[]|.canvases[]|.otherContent[]|."@id"'
-	then
-		echo "Failed to retrieve manifest"
-		exit 1
+fail() {
+	echo "ERROR: $@"
+	clean_up_tmp
+	exit 1
+}
+
+get_anno_pages_from_manifest() {
+	if ! jq -r '.sequences[]|.canvases[]|.otherContent[]|."@id"'; then
+		fail "Failed to find annotation pages"
+	fi
+}
+
+get_full_text_resource_from_anno() {
+	if ! jq -r '.resources[]|select(.dcType=="Page")|.resource."@id"'; then
+		fail "Failed to full text resource"
 	fi
 }
 
@@ -35,9 +54,29 @@ fetch_text() {
 	COLLECTION="$1" # e.g. 9200338
 	RECORD="$2" # e.g. BibliographicResource_3000117673618
 	
-	get_anno_pages $COLLECTION $RECORD | while read ANNOPAGE; do
-			echo Anno page "$ANNOPAGE"
-		done
+	MANIFEST_URL="${API_URL}/presentation/${COLLECTION}/${RECORD}/manifest"
+	
+	TMP_MANIFEST_OUT=$(mktemp)
+	TMP_FILES+=($TMP_MANIFEST_OUT)
+	echo "Getting manifest for ${COLLECTION}/${RECORD}" > /dev/stderr
+	if ! curl -sL "$MANIFEST_URL" > "$TMP_MANIFEST_OUT"; then
+		fail "Failed to read manifest at ${MANIFEST_URL}"
+	else
+		cat "$TMP_MANIFEST_OUT" \
+			| get_anno_pages_from_manifest \
+			| while read ANNOPAGE; do
+				TMP_ANNO_OUT=$(mktemp)
+				TMP_FILES+=($TMP_ANNO_OUT)
+				echo "...Getting annotation page" > /dev/stderr
+				if ! curl -sL "$ANNOPAGE" > "$TMP_ANNO_OUT"; then
+					fail "Failed to read annotation page at ${ANNOPAGE}"
+				else
+					cat "$TMP_ANNO_OUT" \
+						| get_full_text_resource_from_anno
+				fi
+			done
+	fi
+	
 }
 
 main() {
@@ -64,6 +103,7 @@ main() {
         fi
 
         fetch_text $COLLECTION $RECORD
+        clean_up_tmp
 }
 
 main $@
