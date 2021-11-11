0ce25de6e5304b4b036a35dbe06ca11526079d31
Twan Goosen <twan@clarin.eu>
2021-11-11 18:04:26 +0200

    process env viarable an argument to retrieve content

--- fulltext-aggregation/image/src/__main__.py
+++ fulltext-aggregation/image/src/__main__.py
@@ -1,1 +1,21 @@
-print('Hello, World!')
+import sys
+import os
+
+apiUrl = 'https://www.europeana.eu/api/v2/search.json'
+apiKey = os.environ.get('API_KEY')
+
+if apiKey is None:
+    print("Error: API_KEY variable not set")
+    exit(1)
+
+if len(sys.argv) <= 1:
+    print("ERROR: Provide a set identifier as the first argument")
+    exit(1)
+
+collectionId = sys.argv[1]
+
+collectionItemsUrl = apiUrl+'?wskey='+apiKey + '&query=europeana_collectionName:'+collectionId+'*&rows=10&fields=id'
+
+print(collectionItemsUrl)
+
+# TODO: retrieve collection identifiers
