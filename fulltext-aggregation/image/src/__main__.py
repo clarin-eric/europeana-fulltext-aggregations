c0ea52a2e8a1ae0422faec96fa358e4acfb6d0ef
Twan Goosen <twan@clarin.eu>
2021-11-24 17:37:19 +0100

    paginate through results

--- fulltext-aggregation/image/src/__main__.py
+++ fulltext-aggregation/image/src/__main__.py
@@ -22,17 +22,26 @@ def main():
 
     collection_id = sys.argv[1]
 
-    collection_items = get_collection_items(api_url, api_key, collection_id)
+    rows = 100
+    start = 1
+    total_results = -1
+    ids = []
+    while total_results < 0 or start < total_results:
+        collection_items_response = get_collection_items(api_url, api_key, collection_id, start, rows)
+        total_results = collection_items_response["totalResults"]
+        ids += [item["id"] for item in collection_items_response["items"]]
+        print(f"{len(ids)} ids collected (total: {total_results})")
+        start += rows
 
     # TODO: extract collection identifiers
     # TODO: repeat for all pages
 
-    print(collection_items)
+    print(ids)
 
 
-def get_collection_items(apiUrl, apiKey, collectionId):
-    collection_items_url = f"{apiUrl}?wskey={apiKey}&rows=10&profile=minimal&query=europeana_collectionName:{collectionId}*"
-
+def get_collection_items(api_url, api_key, collection_id, start, rows):
+    collection_items_url = f"{api_url}?wskey={api_key}&start={start}&rows={rows}&profile=minimal" \
+                           + f"&query=europeana_collectionName:{collection_id}*"
     response = requests.get(collection_items_url).text
     return json.loads(response)
 
