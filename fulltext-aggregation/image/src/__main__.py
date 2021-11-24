c054ee364f043313d0769072ffd2e590cf32d735
Twan Goosen <twan@clarin.eu>
2021-11-24 10:03:15 +0100

    refactoring in python code

--- fulltext-aggregation/image/src/__main__.py
+++ fulltext-aggregation/image/src/__main__.py
@@ -3,33 +3,39 @@ import sys
 import requests
 import json
 
-apiUrl = os.environ.get('API_URL')
-apiKey = os.environ.get('API_KEY')
 
-if apiUrl is None:
-    print("ERROR: API_KEY variable not set. Using default.")
-    exit(1)
+def main():
+    api_url = os.environ.get('API_URL')
+    api_key = os.environ.get('API_KEY')
 
-if apiKey is None:
-    print("Error: API_KEY variable not set")
-    exit(1)
+    if api_url is None:
+        print("ERROR: API_URL variable not set. Using default.")
+        exit(1)
 
-if len(sys.argv) <= 1:
-    print("ERROR: Provide a set identifier as the first argument")
-    exit(1)
+    if api_key is None:
+        print("Error: API_KEY variable not set")
+        exit(1)
 
-collectionId = sys.argv[1]
+    if len(sys.argv) <= 1:
+        print("ERROR: Provide a set identifier as the first argument")
+        exit(1)
 
-collectionItemsUrl = apiUrl+'?wskey=' + apiKey\
-                     + '&rows=10&profile=minimal&query=europeana_collectionName:' \
-                     + collectionId+'*'
+    collection_id = sys.argv[1]
 
-response = requests.get(collectionItemsUrl).text
-json = json.loads(response)
+    collection_items = get_collection_items(api_url, api_key, collection_id)
 
-# TODO: extract collection identifiers
-# TODO: repeat for all pages
+    # TODO: extract collection identifiers
+    # TODO: repeat for all pages
 
-print(json)
+    print(collection_items)
 
 
+def get_collection_items(apiUrl, apiKey, collectionId):
+    collection_items_url = f"{apiUrl}?wskey={apiKey}&rows=10&profile=minimal&query=europeana_collectionName:{collectionId}*"
+
+    response = requests.get(collection_items_url).text
+    return json.loads(response)
+
+
+if __name__ == "__main__":
+    main()
