bb372347dc27232798860f41316ddf7d96b90196
Twan Goosen <twan@clarin.eu>
2021-11-24 18:02:42 +0100

    cursor based pagination

--- fulltext-aggregation/image/src/__main__.py
+++ fulltext-aggregation/image/src/__main__.py
@@ -2,6 +2,7 @@ import os
 import sys
 import requests
 import json
+import urllib
 
 
 def main():
@@ -22,16 +23,19 @@ def main():
 
     collection_id = sys.argv[1]
 
-    rows = 100
-    start = 1
+    rows = 50
+    cursor = "*"
     total_results = -1
     ids = []
-    while total_results < 0 or start < total_results:
-        collection_items_response = get_collection_items(api_url, api_key, collection_id, start, rows)
-        total_results = collection_items_response["totalResults"]
+    while cursor is not None:
+        collection_items_response = get_collection_items(api_url, api_key, collection_id, rows, cursor)
+        if "error" in collection_items_response:
+            print(f"Error: {collection_items_response['error']}")
+            exit(1)
+        cursor = collection_items_response.get("nextCursor")
         ids += [item["id"] for item in collection_items_response["items"]]
-        print(f"{len(ids)} ids collected (total: {total_results})")
-        start += rows
+        print(f"{len(ids)} ids collected (cursor: {cursor})")
+
 
     # TODO: extract collection identifiers
     # TODO: repeat for all pages
@@ -39,9 +43,14 @@ def main():
     print(ids)
 
 
-def get_collection_items(api_url, api_key, collection_id, start, rows):
-    collection_items_url = f"{api_url}?wskey={api_key}&start={start}&rows={rows}&profile=minimal" \
-                           + f"&query=europeana_collectionName:{collection_id}*"
+def get_collection_items(api_url, api_key, collection_id, rows, cursor):
+    params = {
+        'wskey': api_key,
+        'rows': rows,
+        'cursor': cursor,
+        'query': f"europeana_collectionName:{collection_id}*",
+        'profile': 'minimal'}
+    collection_items_url = f"{api_url}?{urllib.parse.urlencode(params)}"
     response = requests.get(collection_items_url).text
     return json.loads(response)
 
