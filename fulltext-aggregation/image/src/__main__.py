d629c11caee59ea4b5346c4d5059604c5165fed0
Twan Goosen <twan@clarin.eu>
2021-11-25 13:56:06 +0100

    full text content is retrieved and stored

--- fulltext-aggregation/image/src/__main__.py
+++ fulltext-aggregation/image/src/__main__.py
@@ -2,11 +2,11 @@ import os
 import sys
 import requests
 import json
-import re
 import urllib
 import logging
 
-do_save_metadata = False
+do_save_metadata = True
+records_limit = 10
 
 logger = logging.getLogger(__name__)
 
@@ -28,34 +28,36 @@ def main():
         exit(1)
 
     collection_id = sys.argv[1]
-    target_dir = f"{output_base_dir}/{collection_id}"
-    os.makedirs(name=target_dir, exist_ok=True)
 
-    ids = retrieve_and_store_records(search_api_key, search_api_url, target_dir, collection_id)
-    retrieve_full_text(iiif_api_url, ids)
+    metadata_dir = f"{output_base_dir}/{collection_id}/metadata"
+    os.makedirs(name=metadata_dir, exist_ok=True)
 
-    print(ids)
+    ids = retrieve_and_store_records(search_api_key, search_api_url, collection_id, metadata_dir)
+
+    fulltext_dir = f"{output_base_dir}/{collection_id}/fulltext"
+    os.makedirs(name=fulltext_dir, exist_ok=True)
+
+    retrieve_and_store_full_text(iiif_api_url, ids, fulltext_dir)
+
+    # TODO: create CMDI metadata for full text collection
 
 
-def retrieve_and_store_records(api_key, api_url, target_dir, collection_id):
+def retrieve_and_store_records(api_key, api_url, collection_id, target_dir):
     """
     Retrieves the records for all objects in the specified collection, stores the JSON representations to disk
     and produces a list of identifiers for all retrieved records
     :param api_key:
     :param api_url:
-    :param target_dir:
     :param collection_id:
+    :param target_dir:
     :return: list of identifiers
     """
     logger.info(f"Starting retrieval of record ids from collection {collection_id} from API at {api_url}")
 
-    metadata_dir = f"{target_dir}/metadata"
-    os.makedirs(name=metadata_dir, exist_ok=True)
-
     rows = 50
     cursor = "*"
     ids = []
-    while cursor is not None:
+    while cursor is not None and (records_limit is None or len(ids) < records_limit):
         collection_items_response = retrieve_records(api_url, api_key, collection_id, rows, cursor)
         if "error" in collection_items_response:
             print(f"Error: {collection_items_response['error']}")
@@ -65,8 +67,12 @@ def retrieve_and_store_records(api_key, api_url, target_dir, collection_id):
         ids += [item["id"] for item in items]
         logger.debug(f"{len(ids)} ids collected so far (cursor: {cursor})")
         if do_save_metadata:
-            save_records_to_file(metadata_dir, items)
-    logger.info(f"Done. Collected {len(ids)} ids from collection {collection_id}")
+            save_records_to_file(target_dir, items)
+
+    if records_limit is not None and len(ids) > records_limit:
+        ids = ids[0:records_limit]
+
+    logger.info(f"Done. Collected metadata and ids for {len(ids)} records from collection {collection_id}")
     return ids
 
 
@@ -81,11 +87,46 @@ def retrieve_records(api_base_url, api_key, collection_id, rows, cursor):
     return get_json_from_http(collection_items_url)
 
 
-def retrieve_full_text(api_base_url, ids):
+def retrieve_and_store_full_text(api_base_url, ids, target_dir):
+    logger.info(f"Starting retrieval of fulltext for {len(ids)} records from API at {api_base_url}")
     for record_id in ids:
+        logger.info(f"Retrieving fulltext content for {record_id}")
         iiif_manifest_url = f"{api_base_url}/presentation{record_id}/manifest"
         logger.debug(f"Getting manifest for {record_id} from {iiif_manifest_url}")
         manifest = get_json_from_http(iiif_manifest_url)
+
+        annotation_urls = []
+        for sequence in manifest.get("sequences", []):
+            for canvas in sequence.get("canvases", []):
+                for otherContent in canvas.get("otherContent", []):
+                    if isinstance(otherContent, str) and otherContent.startswith(api_base_url):
+                        annotation_urls += [otherContent]
+
+        if len(annotation_urls) > 0:
+            logger.debug(f"Annotation urls: {annotation_urls}")
+            logger.info(f"Found {len(annotation_urls)} annotation URLs. Retrieving content...")
+            annotations = retrieve_full_text_annotations(annotation_urls)
+            if len(annotations) > 0:
+                save_annotations_to_file(target_dir, {record_id: annotations})
+        else:
+            logger.warning(f"No full-text annotation URLs found in manifest for {record_id}")
+
+
+def retrieve_full_text_annotations(annotation_urls):
+    text_annotations = []
+    for url in annotation_urls:
+        logger.debug(f"Retrieving annotation resources from {url}")
+        response = get_json_from_http(url)
+        for resource in response.get("resources", []):
+            if resource.get("dcType") == "Page":
+                logger.debug(f"Page level annotation resource found: {resource}")
+                resource_id = resource.get("resource", {}).get("@id")
+                if resource_id is None:
+                    logger.error("Resource ID not found!")
+                else:
+                    response = get_json_from_http(resource_id)
+                    text_annotations += [response.get("value")]
+    return text_annotations
 
 
 def get_json_from_http(url):
@@ -104,6 +145,16 @@ def save_records_to_file(target_dir, items):
         logger.debug(f"Storing metadata in {file_name}")
         with open(file_name, "w") as text_file:
             text_file.write(json.dumps(item))
+
+
+def save_annotations_to_file(target_dir, annotations_dict):
+    for record_id in annotations_dict:
+        annotations = annotations_dict[record_id]
+        file_name = f"{target_dir}/{id_to_filename(record_id)}.txt"
+        logger.debug(f"Storing fulltext content in {file_name}")
+        with open(file_name, "w") as text_file:
+            for annotation in annotations:
+                text_file.write(annotation)
 
 
 def id_to_filename(value):
