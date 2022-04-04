4846a3b2db15668d965731fed8d6f0609c0434af
Twan Goosen <twan@clarin.eu>
2022-04-04 16:12:34 +0300

    moved metadata aggregation scripts to metadata subdir

---
+++ metadata/image/src/aggregate_collection.py
@@ -0,0 +1,335 @@
+import json
+import logging
+import os
+import requests
+import time
+import re
+
+from glom import glom, PathAccessError, flatten
+from lxml import etree
+from multiprocessing import Pool, Manager
+
+from aggregation_cmdi_creation import make_cmdi_record, make_cmdi_template
+from aggregation_cmdi_creation import make_collection_record, make_collection_record_template
+from common import log_progress
+from common import get_json_from_http
+from common import xpath, xpath_text_values
+from common import normalize_issue_title, normalize_identifier, date_to_year, filename_safe, unique_filename
+
+from common import ALL_NAMESPACES
+from env import FILE_PROCESSING_THREAD_POOL_SIZE, IIIF_API_URL, RECORD_API_URL, RECORD_API_KEY, PRETTY_CMDI_XML
+
+logger = logging.getLogger(__name__)
+
+EDM_ID_PATTERN = re.compile(r'^[A-z]+://data.europeana.eu/item/([^/]+/[^/]+)$')
+
+
+def aggregate(collection_id, metadata_dir, output_dir):
+    start_time = time.time()
+
+    logging.basicConfig()
+    logger.setLevel(logging.INFO)
+    logger.info(f"Start time: {time.strftime('%c', time.localtime(start_time))}")
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    # 'index' metadata records based on properties
+    logger.info("Making index for metadata")
+    index = make_md_index(metadata_dir)
+
+    # save index to file in output directory
+    logger.info(f"Writing index to file")
+    save_index(index, f"{output_dir}/index.json")
+
+    # generate CMDI for the indexed property combinations
+    logger.info(f"Creating CMDI record for items in index in {output_dir}")
+    generate_cmdi_records(collection_id, index, metadata_dir, output_dir)
+
+    end_time = time.time()
+
+    logger.info(f"Aggregation of {collection_id} completed in {end_time - start_time:,.2f} seconds")
+    return index
+
+
+def make_md_index(metadata_dir):
+    md_index = {}
+    files = os.listdir(metadata_dir)
+    logger.info(f"Reading metadata from {len(files)} files in {metadata_dir}")
+    total = len(files)
+
+    with Manager() as manager:
+        part_of_title_map = manager.dict()
+        map_lock = manager.Lock()
+        with requests.Session() as session:
+            indexer = FileProcessor(md_index, metadata_dir, session, total, part_of_title_map, map_lock)
+            with Pool(int(FILE_PROCESSING_THREAD_POOL_SIZE)) as p:
+                data = p.map(indexer.process, files)
+
+        for item in data:
+            # non-matching files yield no response
+            if item is not None:
+                add_to_index(md_index,
+                             identifier=item['identifier'],
+                             titles=item['titles'],
+                             years=item['years'],
+                             filename=item['filename'],
+                             manifest_urls=item['manifest_urls'])
+
+    return md_index
+
+
+class FileProcessor:
+
+    def __init__(self, md_index, metadata_dir, session, total, part_of_title_map, map_lock):
+        self.md_index = md_index
+        self.metadata_dir = metadata_dir
+        self.session = session
+        self.total = total
+        self.count = 0
+        self.last_log = 0
+
+        self.part_of_title_map = part_of_title_map
+        self.map_lock = map_lock
+
+    def process(self, filename):
+        if filename.endswith(".xml"):
+            file_path = f"{self.metadata_dir}/{filename}"
+            logging.debug(f"Processing metadata file {file_path}")
+            return self.process_file(file_path, filename)
+
+        self.count += 1
+        self.last_log = log_progress(None, self.total, self.count, self.last_log,
+                                     category="Reading metadata files",
+                                     interval=1)
+
+    def process_file(self, file_path, filename):
+        try:
+            doc = etree.parse(file_path)
+            identifiers = xpath_text_values(doc, '/rdf:RDF/ore:Proxy/dc:identifier')
+            if len(identifiers) == 0:
+                logger.error(f"No identifier in {file_path}")
+            else:
+                identifier = normalize_identifier(identifiers[0])
+                titles = self.get_titles(doc)
+                years = [date_to_year(date)
+                         for date in xpath_text_values(doc, '/rdf:RDF/ore:Proxy/dcterms:issued')]
+
+                # ideally we can get the IIIF manifest URL
+                iiif_manifest_urls = xpath(doc, '/rdf:RDF/edm:WebResource/dcterms:isReferencedBy/@rdf:resource')
+                if iiif_manifest_urls is not None:
+                    manifest_urls = [ref for ref in set(iiif_manifest_urls) if ref.startswith(IIIF_API_URL)]
+
+                # if this does not work, we can construct one based on the CHO id
+                if manifest_urls is None or len(manifest_urls) == 0:
+                    constructed = construct_iiif_reference(doc)
+                    if constructed:
+                        manifest_urls = [constructed]
+
+                return {
+                    'identifier': identifier,
+                    'titles': titles,
+                    'years': years,
+                    'filename': filename,
+                    'manifest_urls': manifest_urls
+                }
+
+        except etree.Error as err:
+            logger.error(f"Error processing XML document: {err=}")
+
+    def get_titles(self, doc):
+        # look up (newspaper) collection title (or use previously looked up)
+        part_of_refs = xpath(doc, '/rdf:RDF/ore:Proxy/dcterms:isPartOf/@rdf:resource')
+        titles = [self.look_up_title(ref) for ref in part_of_refs if ref is not None]
+        titles = [title for title in titles if title is not None]
+        if titles and len(titles) > 0:
+            return titles
+
+        # use normalized issue title(s)
+        return [normalize_issue_title(title)
+                for title
+                in xpath_text_values(doc, '/rdf:RDF/edm:ProvidedCHO/dc:title')]
+
+    def look_up_title(self, ref):
+        # mplog = log_to_stderr()
+        # mplog.setLevel(logging.INFO)
+        with self.map_lock:
+            from_map = self.part_of_title_map.get(ref, None)
+            if from_map:
+                # fetch title from map (cache)
+                return from_map
+            else:
+                # retrieve title from API
+                match = EDM_ID_PATTERN.match(ref)
+                if match:
+                    edm_id = match.group(1)
+                    url = f"{RECORD_API_URL}/{edm_id}.json?wskey={RECORD_API_KEY}"
+                    # mplog.info(f"Getting collection record from {url}")
+                    json_doc = get_json_from_http(url)
+                    if json_doc is not None:
+                        proxies = glom(json_doc, 'object.proxies', default=None, skip_exc=PathAccessError)
+                        if proxies:
+                            for proxy in proxies:
+                                titles = glom(proxy, 'dcTitle.def', default=None, skip_exc=PathAccessError)
+                                if titles and len(titles) > 0:
+                                    title = titles[0]
+                                    # mplog.info(f"Setting title in map: {ref} -> {title}")
+                                    self.part_of_title_map[ref] = title
+                                    return title
+
+
+def add_to_index(index, identifier, titles, years, filename, manifest_urls):
+    for title in titles:
+        if title not in index:
+            index[title] = {}
+        for year in years:
+            if year not in index[title]:
+                index[title][year] = {}
+            index[title][year][identifier] = {
+                'file': filename,
+                'manifest_urls': manifest_urls
+            }
+
+
+def save_index(index, index_filename):
+    with open(index_filename, 'w') as output_file:
+        json.dump(index, output_file, indent=True)
+
+
+def generate_cmdi_records(collection_id, index, metadata_dir, output_dir):
+    os.makedirs(output_dir, exist_ok=True)
+    template = make_cmdi_template()
+    collection_template = make_collection_record_template()
+    total = sum([len(index[title]) for title in index])
+    count = 0
+    last_log = 0
+
+    filenames_history = []
+    for title in index:
+        files_for_years = {}
+        years = index[title]
+        for year in years:
+            # for each year there is a dict of identifier -> {file, annotation_ref[]}
+            records = years[year]
+
+            if file_created := generate_cmdi_record(records, collection_id, title, year,
+                                                    output_dir, metadata_dir, template, filenames_history):
+                files_for_years[year] = file_created
+
+            count += 1
+            last_log = log_progress(logger, total, count, last_log,
+                                    category="Generating CMDI records",
+                                    interval=1)
+
+        logger.info(f"{len(files_for_years)} year records generated for title '{title}'")
+
+        if len(files_for_years) > 1:
+            # join records from all years
+            title_records = {}
+            for year_records in years.values():
+                title_records.update(year_records)
+            # Make a 'parent' record for the title that links to all years
+            logger.info(f"Generating collection record for title '{title}'")
+            generate_collection_record(title_records, collection_id, title, files_for_years,
+                                       output_dir, metadata_dir, collection_template, filenames_history)
+
+
+def generate_cmdi_record(records, collection_id, title, year, output_dir, metadata_dir, template, previous_filenames):
+    file_name = f"{unique_filename(filename_safe(title + '_' + year), previous_filenames)}.xml"
+    file_path = f"{output_dir}/{file_name}"
+    logger.debug(f"Generating metadata file {file_path}")
+    if cmdi_file := make_cmdi_record(file_name, template, collection_id, title, year, records, metadata_dir):
+        write_xml_tree_to_file(cmdi_file, file_path)
+        return file_name
+
+
+def generate_collection_record(input_records, collection_id, title, year_files, output_dir,
+                               metadata_dir, template, previous_filenames):
+    file_name = f"{unique_filename(filename_safe(title + '_collection'), previous_filenames)}.xml"
+    file_path = f"{output_dir}/{file_name}"
+    logger.debug(f"Generating metadata file {file_path}")
+    if cmdi_file := make_collection_record(file_name, template, collection_id, title, year_files, input_records, metadata_dir):
+        write_xml_tree_to_file(cmdi_file, file_path)
+
+
+def write_xml_tree_to_file(cmdi_file, file_name):
+    # wrap up and write to file
+    if PRETTY_CMDI_XML:
+        etree.indent(cmdi_file, space="  ", level=0)
+    etree.cleanup_namespaces(cmdi_file, top_nsmap=ALL_NAMESPACES)
+    cmdi_file.write(file_name,
+                    encoding='utf-8',
+                    xml_declaration=True,
+                    pretty_print=PRETTY_CMDI_XML)
+
+
+def collect_fulltext_ids(fulltext_dir):
+    ids = {}
+    files = os.listdir(fulltext_dir)
+    total = len(files)
+    count = 0
+    last_log = 0
+    for filename in files:
+        if filename.endswith(".xml"):
+            file_path = f"{fulltext_dir}/{filename}"
+            identifier = extract_fulltext_record_id(file_path)
+            if identifier is not None:
+                logger.debug(f"Extracted identifier {identifier}")
+                ids[normalize_identifier(identifier)] = filename
+        count += 1
+        last_log = log_progress(logger, total, count, last_log,
+                                category="Collecting identifiers from fulltext",
+                                interval=5)
+
+    return ids
+
+
+def extract_fulltext_record_id(file_path):
+    target_element = '{http://www.w3.org/1999/02/22-rdf-syntax-ns#}RDF'
+    context = etree.iterparse(file_path,
+                              events=('end',), tag=target_element,
+                              huge_tree=True)
+    for event, element in context:
+        if element.tag == target_element:
+            xml_base = element.get("{http://www.w3.org/XML/1998/namespace}base")
+            if xml_base is None:
+                logger.error(f"Expecting identifier in @xml:base of root, but not found in {file_path}")
+                return None
+            else:
+                return xml_base
+        element.clear()
+    return None
+
+
+# --------- Helpers ---------
+
+
+def construct_iiif_reference(doc):
+    cho_identifier = xpath(doc, '/rdf:RDF/edm:ProvidedCHO/@rdf:about')
+    if len(cho_identifier) == 1:
+        # example identifier value:
+        # http://data.europeana.eu/item/9200356/BibliographicResource_3000100359046
+        match = re.search(r'\/([^\/]+\/[^\/]+)$', cho_identifier[0])
+        if match:
+            # example manifest URL:
+            # https://iiif.europeana.eu/presentation/9200356/BibliographicResource_3000100359046/manifest
+            return f"{IIIF_API_URL}/presentation/{match.group(1)}/manifest"
+    return None
+
+
+def test_run():
+    collection_id = '9200396'
+    # result = aggregate(collection_id,
+    #                    metadata_dir=f"./test-input/{collection_id}",
+    #                    output_dir=f"./test-output/{collection_id}")
+
+    with open(f"./test-input/{collection_id}/index.json", 'r') as index_file:
+        index = json.load(index_file)
+
+    generate_cmdi_records(collection_id, index,
+                          metadata_dir=f"./test-input/{collection_id}",
+                          output_dir=f"./test-output/{collection_id}")
+    # pprint.pprint(result)
+
+
+if __name__ == "__main__":
+    test_run()
---
+++ metadata/image/src/common.py
@@ -0,0 +1,220 @@
+import os
+import re
+import logging
+import unidecode
+import requests
+import json
+
+DEFAULT_OUTPUT_DIRECTORY = "./output"
+
+DEFAULT_USER_AGENT = 'clarin-fulltext-aggregator/1.0'
+
+CMD_NS = 'http://www.clarin.eu/cmd/1'
+CMDP_NS_RECORD = 'http://www.clarin.eu/cmd/1/profiles/clarin.eu:cr1:p_1633000337997'
+CMDP_NS_COLLECTION_RECORD = 'http://www.clarin.eu/cmd/1/profiles/clarin.eu:cr1:p_1639731773869'
+
+EDM_NAMESPACES = {
+    'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+    'ore': 'http://www.openarchives.org/ore/terms/',
+    'edm': 'http://www.europeana.eu/schemas/edm/',
+    'dc': 'http://purl.org/dc/elements/1.1/',
+    'dcterms': 'http://purl.org/dc/terms/',
+}
+
+CMD_NAMESPACES = {
+    'cmd': CMD_NS,
+    'cmdp': CMDP_NS_RECORD,
+    'cmdp_c': CMDP_NS_COLLECTION_RECORD
+}
+
+ALL_NAMESPACES = {**EDM_NAMESPACES, **CMD_NAMESPACES}
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+user_agent_string = None
+
+
+def id_to_filename(value):
+    return value.replace('/', '_')
+
+
+def get_mandatory_env_var(name):
+    value = os.environ.get(name)
+    if value is None:
+        print(f"ERROR: mandatory {name} variable not set")
+        exit(1)
+    else:
+        logger.info(f'Environment provides value {name}={value}')
+        return value
+
+
+def get_optional_env_var(name, default=None):
+    value = os.environ.get(name)
+    if value is None:
+        logger.info(f'Using default value {name}={default}')
+        return default
+    else:
+        logger.info(f'Environment provides value {name}={value}')
+        return value
+
+
+def get_metadata_dir(basedir, collection_id):
+    return f"{basedir}/{collection_id}/metadata"
+
+
+def get_fulltext_dir(basedir, collection_id):
+    return f"{basedir}/{collection_id}/fulltext"
+
+
+def xpath(tree, path, namespaces=None):
+    if namespaces is None:
+        # default to all namespaces
+        namespaces = ALL_NAMESPACES
+    return tree.xpath(path, namespaces=namespaces)
+
+
+def xpath_text_values(tree, path, namespaces=None):
+    nodes = xpath(tree, path, namespaces)
+    if nodes is None:
+        return []
+    else:
+        return [node.text for node in nodes]
+
+
+def normalize_identifier(identifier):
+    # ex. http://data.theeuropeanlibrary.org/BibliographicResource/3000118435146
+    # ex. http://data.europeana.eu/annotation/9200396/BibliographicResource_3000118435009
+    match = re.search(r"http.*[^\d](\d+)$", identifier)
+    if match:
+        logger.debug(f"Normalised identifier: {identifier} -> {match.group(1)}")
+        return match.group(1)
+    else:
+        logger.warning(f"Identifier {identifier} does not match pattern, skipping normalisation!")
+        return identifier
+
+
+def xml_id(identifier):
+    new_id = identifier
+    if not re.match(r"^[A-z_]", identifier):
+        new_id = "_" + identifier
+    return re.sub('[^A-z0-9_]', '_', new_id)
+
+
+def date_to_year(date):
+    match = re.search(r"(\d{4})-\d{2}-\d{2}", date)
+    if match:
+        return match.group(1)
+    else:
+        return None
+
+
+def is_valid_date(date):
+    return re.match(r"(^\d{4})-\d{2}-\d{2}$", date) is not None
+
+
+def normalize_issue_title(title):
+    match = re.search(r"^[A-z0-9'\"()\s]*[A-z0-9'\"()]", unidecode.unidecode(title))
+    if match:
+        return title[match.start(0):match.end(0)]
+    else:
+        return None
+
+
+def filename_safe(name):
+    return re.sub(r"[^A-z0-9]", '_', name)
+
+
+def unique_filename(name, previous_names):
+    if name in previous_names:
+        original_name = name
+        new_name = name
+        idx = 1
+        while new_name in previous_names:
+            idx += 1
+            new_name = f"{original_name}_{idx}"
+        logger.info(f"Name clash! Renamed '{original_name}' to '{new_name}'")
+    else:
+        new_name = name
+
+    previous_names += [new_name]
+    return new_name
+
+
+def get_unique_xpath_values(docs, path):
+    return list(dict.fromkeys(get_all_xpath_values(docs, path)))
+
+
+def get_all_xpath_values(docs, path):
+    values = []
+    for doc in docs:
+        values += xpath(doc, path)
+    return values
+
+
+def filter_fulltext_ids(ids, fulltext_dir):
+    available_files = os.listdir(fulltext_dir)
+    return list(filter(lambda identifier: id_to_fulltext_file(identifier) in available_files, ids))
+
+
+def id_to_fulltext_file(identifier):
+    return f"{id_to_filename(identifier)}.xml"
+
+
+def get_user_agent():
+    global user_agent_string
+    if user_agent_string is None:
+        user_agent = get_optional_env_var('HTTP_USER_AGENT', DEFAULT_USER_AGENT)
+        user_agent_string = user_agent
+    else:
+        user_agent = user_agent_string
+    return user_agent
+
+
+def http_request(url, session=None):
+    headers = {
+        'User-Agent': get_user_agent()
+    }
+
+    if session is None:
+        return requests.get(url, headers=headers)
+    else:
+        return session.get(url, headers=headers)
+
+
+def get_json_from_http(url, session=None):
+    logger.debug(f"Making request: {url}")
+    response = http_request(url, session)
+    logger.debug(f"API response for {url}: {response}")
+
+    response_content = response.text
+    if response_content is None:
+        logger.error(f"No response or invalid response from {url} ({response.status_code})")
+        return None
+
+    if response.status_code != requests.codes.ok:
+        logger.warning(f'Response status code: {response.status_code}')
+        logger.debug(f'Response content: {response_content[0:100]}...')
+
+    try:
+        return json.loads(response_content)
+    except json.JSONDecodeError:
+        logger.error(f"Error decoding response from {url}")
+
+
+def log_progress(logr, total, current, last_log, category=None, interval_pct=5, interval=-1):
+    if last_log is None:
+        last_log = 0
+
+    if interval < 1:
+        interval = .01 * interval_pct * total
+
+    if current >= last_log + interval:
+        if logr is None:
+            logging.info(f"{category} - Progress: {current}/{total} ({current / total:2.0%})")
+        else:
+            logr.info(f"{category} - Progress: {current}/{total} ({current / total:2.0%})")
+        return current
+    else:
+        return last_log
