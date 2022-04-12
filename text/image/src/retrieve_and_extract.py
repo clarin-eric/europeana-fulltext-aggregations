a39a24866e072bba928ab4442bbbf10acc1b9f00
Twan Goosen <twan@clarin.eu>
2022-04-12 11:35:02 +0300

    extract text from XML and save to file on disk

--- text/image/src/retrieve_and_extract.py
+++ text/image/src/retrieve_and_extract.py
@@ -1,10 +1,25 @@
 import logging
+
+from black import out
 from stream_unzip import stream_unzip
+from io import BytesIO
+from lxml import etree
+import os
 
 logger = logging.getLogger(__name__)
 
 ZIP_BASE_PATH = '/Users/twagoo/Documents/Projects/Europeana/fulltext/dumps/edm-issue'
 ZIP_BASE_FTP_URL = 'ftp://download.europeana.eu/newspapers/fulltext/edm_issue'
+
+xml_parser = etree.XMLParser(resolve_entities=False)
+
+EDM_NAMESPACES = {
+    'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+    'ore': 'http://www.openarchives.org/ore/terms/',
+    'edm': 'http://www.europeana.eu/schemas/edm/',
+    'dc': 'http://purl.org/dc/elements/1.1/',
+    'dcterms': 'http://purl.org/dc/terms/',
+}
 
 
 def main(collection_id, output_dir):
@@ -14,14 +29,37 @@ def main(collection_id, output_dir):
     chunks_generator = zipped_chunks_local(collection_id)
     # chunks_generator = zipped_chunks_ftp(collection_id)
     for file_name, file_size, unzipped_chunks in stream_unzip(chunks_generator):
-        content = bytearray()
-        for chunk in unzipped_chunks:
-            content += chunk
-            logger.debug(f'Getting chunk for {file_name}')
-        logger.info(f'Read file from zip: {file_name} ({len(content)} bytes)')
-        xml = bytes(content)
-        logger.debug(f'Content: {xml}')
-        break
+
+        logger.info(f'Reading file from zip: {file_name}')
+        xml = read_file_from_zip(file_name, unzipped_chunks)
+        text = extract_text(BytesIO(xml))
+
+        write_to_file(text, output_dir, file_name)
+
+
+def read_file_from_zip(file_name, chunks):
+    content = bytearray()
+    for chunk in chunks:
+        content += chunk
+        logger.debug(f'Getting chunk for {file_name}')
+    xml = bytes(content)
+    return xml
+
+
+def extract_text(source):
+    tree = etree.parse(source, xml_parser)
+    node = tree.xpath('/rdf:RDF/edm:FullTextResource/rdf:value', namespaces=EDM_NAMESPACES)
+    if len(node) > 0:
+        return node[0].text
+
+
+def write_to_file(text, output_dir, file_name):
+    file_name_base = os.path.splitext(file_name.decode())[0]
+    output_file = f'{output_dir}/{file_name_base}.txt'
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+    with open(output_file, 'w') as f:
+        logger.info(f'Writing text to {os.path.realpath(output_file)}')
+        f.write(text)
 
 
 def zipped_chunks_local(collection_id):
