f226220f0e846dcf4574bf0416c1a65783f31d4a
Twan Goosen <twan@clarin.eu>
2022-04-12 15:15:24 +0300

    FTP retrieval

--- text/image/src/retrieve_and_extract.py
+++ text/image/src/retrieve_and_extract.py
@@ -1,18 +1,20 @@
 import logging
 import os
 import time
+import threading
 
 from stream_unzip import stream_unzip
 from io import BytesIO
 from lxml import etree
-
+from ftplib import FTP
+from urllib.parse import urlparse
+from queue import SimpleQueue
 
 logger = logging.getLogger(__name__)
 
-ZIP_BASE_PATH = '/Users/twagoo/Documents/Projects/Europeana/fulltext/dumps/edm-issue'
-# TODO: get from environment variable DUMP_FTP_BASE_URL
-ZIP_BASE_FTP_URL = 'ftp://download.europeana.eu/newspapers/fulltext/edm_issue'
-
+BLOCK_SIZE = 65536
+ZIP_BASE_PATH = os.environ.get('DUMP_BASE_PATH')
+ZIP_BASE_FTP_URL = os.environ.get('DUMP_FTP_BASE_URL')
 
 xml_parser = etree.XMLParser(resolve_entities=False, huge_tree=True, remove_pis=True)
 
@@ -27,12 +29,16 @@ EDM_NAMESPACES = {
 
 def main(collection_id, output_dir):
     logging.basicConfig()
-    logger.setLevel(logging.INFO)
+    if os.environ.get('DEBUG', default='false').lower() == 'true':
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
 
     start_time = time.perf_counter()
     logger.info(f'Retrieving and extracting fulltext from dump for collection {collection_id}')
-    chunks_generator = zipped_chunks_local(collection_id)
-    # chunks_generator = zipped_chunks_ftp(collection_id)
+
+    chunks_generator = create_dump_chunk_generator(collection_id)
+
     for file_name_b, file_size, unzipped_chunks in stream_unzip(chunks_generator):
         file_name = file_name_b.decode()
         logger.info(f'Reading file from zip: {file_name}')
@@ -46,11 +52,20 @@ def main(collection_id, output_dir):
     logger.info(f'Completed in {time_elapsed/60:0.0f}m{(time_elapsed%60):02.0f}s')
 
 
+def create_dump_chunk_generator(collection_id):
+    if ZIP_BASE_FTP_URL:
+        return zipped_chunks_ftp(collection_id)
+    if ZIP_BASE_PATH:
+        return zipped_chunks_local(collection_id)
+    else:
+        logger.error("No data to process - configure FTP or local path for dump")
+        exit(1)
+
+
 def read_file_from_zip(file_name, chunks):
     content = bytearray()
     for chunk in chunks:
         content += chunk
-        logger.debug(f'Getting chunk for {file_name}')
     xml = bytes(content)
     return xml
 
@@ -71,25 +86,48 @@ def write_to_file(text, output_dir, file_name):
         f.write(text)
 
 
+def zipped_chunks_ftp(collection_id):
+    file = f'{collection_id}.zip'
+    logger.info(f'Opening {ZIP_BASE_FTP_URL}/{file}')
+
+    parsed_url = urlparse(ZIP_BASE_FTP_URL)
+    if parsed_url.scheme != 'ftp':
+        logger.warning(f'Configured base URL is "{parsed_url.scheme}", expecting "ftp"')
+
+    ftp = FTP(parsed_url.hostname)
+    ftp.login()
+    ftp.cwd(parsed_url.path)
+
+    queue = SimpleQueue()
+
+    def ftp_thread_target():
+        ftp.retrbinary(f'RETR {file}', callback=queue.put, blocksize=BLOCK_SIZE)
+        queue.put(None)
+
+    logger.info(f'Starting retrieval from {ftp.host}')
+    ftp_thread = threading.Thread(target=ftp_thread_target)
+    ftp_thread.start()
+
+    count = 0
+    while True:
+        chunk = queue.get()
+        if chunk:
+            if logger.level == logging.DEBUG:
+                count += 1
+                if (count % 100) == 0:
+                    logger.debug(f'Chunk count: {count}. Queue size: {queue.qsize()}.')
+            yield chunk
+        else:
+            return
+
+
 def zipped_chunks_local(collection_id):
     path = f'{ZIP_BASE_PATH}/{collection_id}.zip'
     logger.info(f'Opening {path}')
-    with open(path, mode='rb', buffering=65536) as f:
+    with open(path, mode='rb', buffering=2*BLOCK_SIZE) as f:
         while True:
-            data = f.read(65536)
+            data = f.read(BLOCK_SIZE)
             if data:
                 yield data
             else:
                 break
-
-
-def zipped_chunks_ftp(collection_id):
-    url = f'{ZIP_BASE_FTP_URL}/{collection_id}.zip'
-    logger.info(f'Opening {url}')
-    # TODO: implement
-    # url = f'{ZIP_BASE_URL}/{collection_id}.zip'
-    # # Iterable that yields the bytes of a zip file
-    # with httpx.stream('GET', url) as r:
-    #     yield from r.iter_bytes(chunk_size=65536)
-
-
--- text/image/src/script/entrypoint.sh
+++ text/image/src/script/entrypoint.sh
@@ -6,7 +6,7 @@ DEBUG="${DEBUG:-false}"
 OUTPUT_DIR="${OUTPUT_DIR:-/output}"
 DUMP_FTP_BASE_URL="${DUMP_FTP_BASE_URL:-ftp://download.europeana.eu/newspapers/fulltext/edm_issue}"
 
-cd /app/src
+cd /app
 
 python3 '__main__.py' "${COLLECTION_ID}" "${OUTPUT_DIR}"
 
